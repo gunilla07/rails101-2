70e76a4f8deb72dc54e97c46ecde4fa656ee1741
gunilla <gunilla1148@gmail.com>
2017-04-01 20:22:54 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -43,7 +44,7 @@ end
   end
 
   def destroy
-    
+
 
     @group.destroy
 
